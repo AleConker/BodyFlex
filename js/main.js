3adfcc8445b66901b57344758931c13331c633aa
AleConker <dguapacha@estudiante.uniajc.edu.co>
2024-05-01 21:09:44 -0500

    Segundoo Commit

---
+++ js/main.js
@@ -0,0 +1,6 @@
+var btnRegistro = document.getElementsByClassName("btn_registro_home");
+var btnBuscador = document.getElementsByClassName("btn_buscador_home");
+
+btnRegistro.addEventListener('click', function() {
+    window.location.href = './vistas/registro.html';
+})
